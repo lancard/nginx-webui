c532e8dc2633496ed3909c1a5a2c5d00f1914c6d
root <root@localhost>
2024-05-21 02:37:03 +0000

    change dns server

--- admin/shell/dns-challenge-start.sh
+++ admin/shell/dns-challenge-start.sh
@@ -5,7 +5,7 @@ echo -n $CERTBOT_VALIDATION > /tmp/$CERTBOT_DOMAIN
 # wait until dig complete
 while :
 do
-    dig -t txt @1.1.1.1 _acme-challenge.$CERTBOT_DOMAIN +short | grep "$CERTBOT_VALIDATION" > /dev/null
+    dig -t txt @8.8.8.8 _acme-challenge.$CERTBOT_DOMAIN +short | grep "$CERTBOT_VALIDATION" > /dev/null
     if [ $? -eq 0 ]
     then
         break
