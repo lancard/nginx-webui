95b5ae6a64f7a53974ce6b9474eaf15878937b45
root <root@localhost>
2024-05-21 02:57:52 +0000

    add wait DNS TTL

--- admin/shell/dns-challenge-start.sh
+++ admin/shell/dns-challenge-start.sh
@@ -8,6 +8,8 @@ do
     dig -t txt @8.8.8.8 _acme-challenge.$CERTBOT_DOMAIN +short | grep "$CERTBOT_VALIDATION" > /dev/null
     if [ $? -eq 0 ]
     then
+        # wait DNS TTL
+        sleep 120
         break
     fi
 
